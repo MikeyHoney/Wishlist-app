30555f7f28409d39f64300165c52d25afc5f916f
michaelh1@muskingum.edu <michaelh1@muskingum.edu>
2023-07-20 23:26:46 +0000

    Edited HomeForm

--- client_code/HomeForm/__init__.py
+++ client_code/HomeForm/__init__.py
@@ -35,7 +35,7 @@ class HomeForm(HomeFormTemplate):
     self.HomeLink.visible = bool(user)
     self.ListLink.visible = bool(user)
     self.LoginLink.visible = bool(not user)
-    #self.logout_link_click.visible = bool(user)
+    #self.logout.visible = bool(user)
 
   def list_link(self, **event_args):
     routing.set_url_hash('List')
