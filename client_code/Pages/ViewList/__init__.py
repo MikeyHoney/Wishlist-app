b9c72fff1bb80446dc0a8451f2c4b3263f58902e
michaelh1@muskingum.edu <michaelh1@muskingum.edu>
2024-01-18 18:00:13 +0000

    Created Pages.EditGift and Pages.ViewList; Deleted Pages.ViewGift

--- client_code/Pages/ViewList/__init__.py
+++ client_code/Pages/ViewList/__init__.py
@@ -1,4 +1,4 @@
-from ._anvil_designer import ViewGiftTemplate
+from ._anvil_designer import ViewListTemplate
 from anvil import *
 import anvil.server
 import anvil.users
@@ -8,7 +8,7 @@ import anvil.tables as tables
 import anvil.tables.query as q
 from anvil.tables import app_tables
 
-class ViewGift(ViewGiftTemplate):
+class ViewList(ViewListTemplate):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
