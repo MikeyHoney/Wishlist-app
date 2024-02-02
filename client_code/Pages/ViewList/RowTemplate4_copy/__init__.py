0c799af0a29d962ce0e02a174715676185e1439d
michaelh1@muskingum.edu <michaelh1@muskingum.edu>
2024-02-02 16:59:35 +0000

    Created Pages.ViewList.RowTemplate4_copy; Edited Pages.ViewList and Pages.Gift.RowTemplate4

---
+++ client_code/Pages/ViewList/RowTemplate4_copy/__init__.py
@@ -0,0 +1,24 @@
+from ._anvil_designer import RowTemplate4_copyTemplate
+from anvil import *
+import anvil.server
+import anvil.users
+import anvil.google.auth, anvil.google.drive
+from anvil.google.drive import app_files
+import anvil.tables as tables
+import anvil.tables.query as q
+from anvil.tables import app_tables
+
+class RowTemplate4_copy(RowTemplate4_copyTemplate):
+  def __init__(self, **properties):
+    self.init_components(**properties)
+
+
+
+  def edit_row(self, **event_args):
+    #anvil.server.call('open_edit_form', list_id=self.item['List_Id'])\
+    #gift_id = self.item['Gift_Id']
+    self.parent.raise_event('x-edit', gift=self.item)
+
+
+  def delete_row_click(self, **event_args):
+    self.parent.raise_event('x-delete', gift=self.item)
